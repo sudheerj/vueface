813215de4efb692adfbc63c47d3520eeb2064665
sudheerj <jsudheer50@gmail.com>
2017-12-13 11:19:26 +0800

    Demo cosmetics

--- showcase/src/components/sidebar.vue
+++ showcase/src/components/sidebar.vue
@@ -1,130 +1,132 @@
 <template>
-  <div id="layout-sidebar">
+  <div id="layout-sidebar" :class="{'active': menuActive}">
     <div class="layout-menu">
-      <a id="menu_input" ref="inputmenutitle" :class="{ 'active-menuitem':$refs.inputmenutitle == activeMenuId}"
-         @click="activeMenuId = (activeMenuId === $refs.inputmenutitle ? null : $refs.inputmenutitle)">
+      <a id="menu_input" ref="inputmenutitle" :class="{'active-menuitem':(element.inputmenutitle) == activeMenuId}"
+         @click="activeMenuId = (activeMenuId === element.inputmenutitle ? null : element.inputmenutitle)">
         <img src="../assets/images/icons/input-active.svg" class="layout-menu-icon-active"/>
         <img src="../assets/images/icons/input.svg" class="layout-menu-icon-inactive"/>
         <span>Input</span>
       </a>
 
       <div
-        :class="{submenushow: ($refs.inputmenutitle == activeMenuId), 'submenuhide': ($refs.inputmenutitle != activeMenuId)}">
-        <router-link to="/components/inputtext">InputText</router-link>
-        <router-link to="/components/inputtextarea">InputTextarea</router-link>
-        <router-link to="/components/password">Password</router-link>
-        <router-link to="/components/inputgroup">InputGroup</router-link>
-        <router-link to="/components/checkbox">Checkbox</router-link>
-        <router-link to="/components/tristatecheckbox">TriStateCheckbox</router-link>
-        <router-link to="/components/togglebutton">ToggleButton</router-link>
-        <router-link to="/components/radiobutton">RadioButton</router-link>
-        <router-link to="/components/editor">Editor</router-link>
-        <router-link to="/components/inplace">Inplace</router-link>
-        <router-link to="/components/selectbutton">SelectButton</router-link>
-        <router-link to="/components/rating">Rating</router-link>
-        <router-link to="/components/inputswitch">InputSwitch</router-link>
-        <router-link to="/components/spinner">Spinner</router-link>
+        :class="{submenushow: (element.inputmenutitle == activeMenuId), 'submenuhide': (element.inputmenutitle != activeMenuId)}">
+        <span @click="menuActive = false">
+        <router-link to="/components/inputtext" >&#9679; InputText</router-link>
+          </span>
+        <router-link to="/components/inputtextarea">&#9679; InputTextarea</router-link>
+        <router-link to="/components/password">&#9679; Password</router-link>
+        <router-link to="/components/inputgroup">&#9679; InputGroup</router-link>
+        <router-link to="/components/checkbox">&#9679; Checkbox</router-link>
+        <router-link to="/components/tristatecheckbox">&#9679; TriStateCheckbox</router-link>
+        <router-link to="/components/togglebutton">&#9679; ToggleButton</router-link>
+        <router-link to="/components/radiobutton">&#9679; RadioButton</router-link>
+        <router-link to="/components/editor">&#9679; Editor</router-link>
+        <router-link to="/components/inplace">&#9679; Inplace</router-link>
+        <router-link to="/components/selectbutton">&#9679; SelectButton</router-link>
+        <router-link to="/components/rating">&#9679; Rating</router-link>
+        <router-link to="/components/inputswitch">&#9679; InputSwitch</router-link>
+        <router-link to="/components/spinner">&#9679; Spinner</router-link>
       </div>
 
-      <a id="button_menutitle" ref="buttonmenutitle" :class="{'active-menuitem': $refs.buttonmenutitle == activeMenuId}"
-         @click="activeMenuId = (activeMenuId === $refs.buttonmenutitle ? null : $refs.buttonmenutitle)">
+      <a id="button_menutitle" ref="buttonmenutitle" :class="{'active-menuitem': element.buttonmenutitle == activeMenuId}"
+         @click="activeMenuId = (activeMenuId === element.buttonmenutitle ? null : element.buttonmenutitle)">
         <img src="../assets/images/icons/button-active.svg" class="layout-menu-icon-active"/>
         <img src="../assets/images/icons/button.svg" class="layout-menu-icon-inactive"/>
         <span>Button</span>
       </a>
       <div
-        :class="{'submenushow': ($refs.buttonmenutitle == activeMenuId), 'submenuhide': ($refs.buttonmenutitle != activeMenuId)}">
-        <router-link to="/components/button">Button</router-link>
+        :class="{'submenushow': (element.buttonmenutitle == activeMenuId), 'submenuhide': (element.buttonmenutitle != activeMenuId)}">
+        <router-link to="/components/button">&#9679; Button</router-link>
       </div>
 
-      <a id="data_menutitle" ref="datamenutitle" :class="{'active-menuitem': $refs.datamenutitle == activeMenuId}"
-         @click="activeMenuId = (activeMenuId === $refs.datamenutitle ? null : $refs.datamenutitle)">
+      <a id="data_menutitle" ref="datamenutitle" :class="{'active-menuitem': element.datamenutitle == activeMenuId}"
+         @click="activeMenuId = (activeMenuId === element.datamenutitle ? null : element.datamenutitle)">
         <img src="../assets/images/icons/data-active.svg" class="layout-menu-icon-active"/>
         <img src="../assets/images/icons/data.svg" class="layout-menu-icon-inactive"/>
         <span>Data</span>
       </a>
       <div
-        :class="{'submenushow': ($refs.datamenutitle == activeMenuId), 'submenuhide': ($refs.datamenutitle != activeMenuId)}">
-        <router-link to="/components/gmap">Gmap</router-link>
+        :class="{'submenushow': (element.datamenutitle == activeMenuId), 'submenuhide': (element.datamenutitle != activeMenuId)}">
+        <router-link to="/components/gmap">&#9679; Gmap</router-link>
       </div>
 
-      <a id="menu_panel" ref="panelmenutitle" :class="{'active-menuitem': $refs.panelmenutitle == activeMenuId}"
-         @click="activeMenuId = (activeMenuId === $refs.panelmenutitle ? null : $refs.panelmenutitle)">
+      <a id="menu_panel" ref="panelmenutitle" :class="{'active-menuitem': element.panelmenutitle == activeMenuId}"
+         @click="activeMenuId = (activeMenuId === element.panelmenutitle ? null : element.panelmenutitle)">
         <img src="../assets/images/icons/panel-active.svg" class="layout-menu-icon-active"/>
         <img src="../assets/images/icons/panel.svg" class="layout-menu-icon-inactive"/>
         <span>Panel</span>
       </a>
       <div
-        :class="{'submenushow': ($refs.panelmenutitle == activeMenuId), 'submenuhide': ($refs.panelmenutitle != activeMenuId)}">
-        <router-link to="/components/panel">Panel</router-link>
-        <router-link to="/components/accordion">Accordion</router-link>
-        <router-link to="/components/fieldset">Fieldset</router-link>
-        <router-link to="/components/grid">Grid</router-link>
-        <router-link to="/components/toolbar">Toolbar</router-link>
+        :class="{'submenushow': (element.panelmenutitle == activeMenuId), 'submenuhide': (element.panelmenutitle != activeMenuId)}">
+        <router-link to="/components/panel">&#9679; Panel</router-link>
+        <router-link to="/components/accordion">&#9679; Accordion</router-link>
+        <router-link to="/components/fieldset">&#9679; Fieldset</router-link>
+        <router-link to="/components/grid">&#9679; Grid</router-link>
+        <router-link to="/components/toolbar">&#9679; Toolbar</router-link>
       </div>
 
-      <a id="menu_overlay" ref="overlaymenutitle" :class="{'active-menuitem': $refs.overlaymenutitle == activeMenuId}"
-         @click="activeMenuId = (activeMenuId === $refs.overlaymenutitle ? null : $refs.overlaymenutitle)">
+      <a id="menu_overlay" ref="overlaymenutitle" :class="{'active-menuitem': element.overlaymenutitle == activeMenuId}"
+         @click="activeMenuId = (activeMenuId === element.overlaymenutitle ? null : element.overlaymenutitle)">
         <img src="../assets/images/icons/overlay-active.svg" class="layout-menu-icon-active"/>
         <img src="../assets/images/icons/overlay.svg" class="layout-menu-icon-inactive"/>
         <span>Overlay</span>
       </a>
       <div
-        :class="{'submenushow': ($refs.overlaymenutitle == activeMenuId), 'submenuhide': ($refs.overlaymenutitle != activeMenuId)}">
-        <router-link to="/components/sidebar">Sidebar</router-link>
+        :class="{'submenushow': (element.overlaymenutitle == activeMenuId), 'submenuhide': (element.overlaymenutitle != activeMenuId)}">
+        <router-link to="/components/sidebar">&#9679; Sidebar</router-link>
       </div>
 
 
-      <a id="menu_chart" ref="chartmenutitle" :class="{'active-menuitem': $refs.chartmenutitle == activeMenuId}"
-         @click="activeMenuId = (activeMenuId === $refs.chartmenutitle ? null : $refs.chartmenutitle)">
+      <a id="menu_chart" ref="chartmenutitle" :class="{'active-menuitem': element.chartmenutitle == activeMenuId}"
+         @click="activeMenuId = (activeMenuId === element.chartmenutitle ? null : element.chartmenutitle)">
         <img src="../assets/images/icons/charts-active.svg" class="layout-menu-icon-active"/>
         <img src="../assets/images/icons/charts.svg" class="layout-menu-icon-inactive"/>
         <span>Charts</span>
       </a>
       <div
-        :class="{'submenushow': ($refs.chartmenutitle == activeMenuId), 'submenuhide': ($refs.chartmenutitle != activeMenuId)}">
-        <router-link to="/components/charts">Charts</router-link>
+        :class="{'submenushow': (element.chartmenutitle == activeMenuId), 'submenuhide': (element.chartmenutitle != activeMenuId)}">
+        <router-link to="/components/charts">&#9679; Charts</router-link>
       </div>
 
       <a id="menu_messages" ref="messagesmenutitle"
-         :class="{'active-menuitem': $refs.messagesmenutitle == activeMenuId}"
-         @click="activeMenuId = (activeMenuId === $refs.messagesmenutitle ? null : $refs.messagesmenutitle)">
+         :class="{'active-menuitem': element.messagesmenutitle == activeMenuId}"
+         @click="activeMenuId = (activeMenuId === element.messagesmenutitle ? null : element.messagesmenutitle)">
         <img src="../assets/images/icons/message-active.svg" class="layout-menu-icon-active"/>
         <img src="../assets/images/icons/message.svg" class="layout-menu-icon-inactive"/>
         <span>Messages</span>
       </a>
       <div
-        :class="{'submenushow': ($refs.messagesmenutitle == activeMenuId), 'submenuhide': ($refs.messagesmenutitle != activeMenuId)}">
-        <router-link to="/components/message">Message</router-link>
-        <router-link to="/components/growl">Growl</router-link>
+        :class="{'submenushow': (element.messagesmenutitle == activeMenuId), 'submenuhide': (element.messagesmenutitle != activeMenuId)}">
+        <router-link to="/components/message">&#9679; Message</router-link>
+        <router-link to="/components/growl">&#9679; Growl</router-link>
       </div>
 
       <a id="menu_multimedia" ref="multimediamenutitle"
-         :class="{'active-menuitem':$refs.multimediamenutitle == activeMenuId}"
-         @click="activeMenuId = (activeMenuId === $refs.multimediamenutitle ? null : $refs.multimediamenutitle)">
+         :class="{'active-menuitem':element.multimediamenutitle == activeMenuId}"
+         @click="activeMenuId = (activeMenuId === element.multimediamenutitle ? null : element.multimediamenutitle)">
         <img src="../assets/images/icons/multimedia-active.svg" class="layout-menu-icon-active"/>
         <img src="../assets/images/icons/multimedia.svg" class="layout-menu-icon-inactive"/>
         <span>Multimedia</span>
       </a>
       <div
-        :class="{'submenushow': ($refs.multimediamenutitle == activeMenuId), 'submenuhide': ($refs.multimediamenutitle != activeMenuId)}">
-        <router-link to="/components/galleria">Galleria</router-link>
+        :class="{'submenushow': (element.multimediamenutitle == activeMenuId), 'submenuhide': (element.multimediamenutitle != activeMenuId)}">
+        <router-link to="/components/galleria">&#9679; Galleria</router-link>
       </div>
 
-      <a id="menu_misc" ref="misctitle" :class="{'active-menuitem':$refs.misctitle == activeMenuId}"
-         @click="activeMenuId = (activeMenuId === $refs.misctitle ? null : $refs.misctitle)">
+      <a id="menu_misc" ref="misctitle" :class="{'active-menuitem':element.misctitle == activeMenuId}"
+         @click="activeMenuId = (activeMenuId === element.misctitle ? null : element.misctitle)">
         <img src="../assets/images/icons/misc-active.svg" class="layout-menu-icon-active"/>
         <img src="../assets/images/icons/misc.svg" class="layout-menu-icon-inactive"/>
         <span>Misc</span>
       </a>
-      <div :class="{'submenushow': ($refs.misctitle == activeMenuId), 'submenuhide': ($refs.misctitle != activeMenuId)}">
-        <router-link to="/components/captcha">Captcha</router-link>
-        <router-link to="/components/progressspinner">ProgressSpinner</router-link>
-        <router-link to="/components/progressbar">ProgressBar</router-link>
-        <router-link to="/components/terminal">Terminal</router-link>
-        <router-link to="/components/blockui">BlockUI</router-link>
-        <router-link to="/components/backtotop">BackToTop</router-link>
-        <router-link to="/components/codehighlighter">CodeHighlighter</router-link>
+      <div :class="{'submenushow': (element.misctitle == activeMenuId), 'submenuhide': (element.misctitle != activeMenuId)}">
+        <router-link to="/components/captcha">&#9679; Captcha</router-link>
+        <router-link to="/components/progressspinner">&#9679;ProgressSpinner</router-link>
+        <router-link to="/components/progressbar">&#9679; ProgressBar</router-link>
+        <router-link to="/components/terminal">&#9679; Terminal</router-link>
+        <router-link to="/components/blockui">&#9679; BlockUI</router-link>
+        <router-link to="/components/backtotop">&#9679; BackToTop</router-link>
+        <router-link to="/components/codehighlighter">&#9679; CodeHighlighter</router-link>
       </div>
 
     </div>
@@ -139,10 +141,23 @@
     data: function () {
       return {
         menuActive: false,
-        activeMenuId: ''
+        activeMenuId: '',
+        element: {}
+      }
+    },
+        mounted: function () {
+          this.element.inputmenutitle = this.$refs.inputmenutitle.id;
+          this.element.buttonmenutitle = this.$refs.buttonmenutitle.id;
+          this.element.datamenutitle = this.$refs.datamenutitle.id;
+          this.element.panelmenutitle = this.$refs.panelmenutitle.id;
+          this.element.overlaymenutitle = this.$refs.overlaymenutitle.id;
+          this.element.chartmenutitle = this.$refs.chartmenutitle.id;
+          this.element.messagesmenutitle = this.$refs.messagesmenutitle.id;
+          this.element.multimediamenutitle = this.$refs.multimediamenutitle.id;
+          this.element.misctitle = this.$refs.misctitle.id;
       }
 
-    }
+
   };
 </script>
 
