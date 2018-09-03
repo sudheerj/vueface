81b9939cc3ba99619fb8d287a361ff4fd0df568e
sudheerj <jsudheer50@gmail.com>
2018-09-03 23:51:09 +0800

    Updates to showcase footer

--- showcase/src/components/footer.vue
+++ showcase/src/components/footer.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="layout-footer">
     <div class="content-section layout-footer">
+      <span>Released under the MIT License</span>
       <div class="footer-links">
         <a href="https://github.com/sudheerj/vueface"><i class="fa fa-github"></i></a>
         <a href="https://twitter.com/SudheerJonna"><i class="fa fa-twitter"></i></a>
