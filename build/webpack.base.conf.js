42a9d2241e5046edf5cf6185fbf533d7f43acbe3
sudheerj <jsudheer50@gmail.com>
2018-09-06 07:29:06 +0800

    Fix image loading in webpack

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -48,7 +48,7 @@ module.exports = {
         include: [resolve('src'), resolve('test')]
       },
       {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        test: /\.(png|jp(e*)g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
